88a6251362d0a4950906abc324b0db1bcc66e4b2
randy <ndokongrandy@yahoo.com>
2020-11-16 01:05:01 +0100

    done

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from 'react-icons/ai';
+
+
+const Question = (props) => {
+  //  %%% variables %%%
+  const {title,info} = props
+  const [fullView,setFullView] = useState(false)
+
+  return (
+    <article className="question">
+      <header className="question">
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setFullView(!fullView)}>
+          {fullView ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle />}
+        </button>
+      </header>
+      {fullView && <p>{info}</p>}
+    </article> 
+  );
 };
 
 export default Question;
